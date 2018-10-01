2a6577a323063e303ca086e90789f2e0c129d870
Raj Saha Choudhury (TECH MAHINDRA LTD.) <v-rassc@microsoft.com>
2018-10-01 21:41:29 +0530

    Signed-off-by: Raj Saha Choudhury (TECH MAHINDRA LTD.) <v-rassc@microsoft.com>

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+
+
+    public int speed = 100;
+	// Use this for initialization
+	void Start () {
+
+    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+        transform.Rotate(Vector3.up * Time.deltaTime* speed);
+
+    }
+}
